ec74721e2bd55e917288323af8eb4ebc64aa1dc1
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-07-15 23:47:37 +0530

    Added Coffee Cups Program

--- Increment_Number/Program.cs
+++ Increment_Number/Program.cs
@@ -1,4 +1,18 @@
-﻿public class Program
+﻿// LBP5
+
+// Return the Next Number from the Integer Passed
+
+// implement a program that takes a number as an argument, increments the number by +1 and returns the result
+
+// input ----------> a number from the user
+// constraints-----> no constraints
+// output ---------> an incremented value
+
+
+
+
+
+public class Program
 {
     public static void Main(string[] args)
     {
