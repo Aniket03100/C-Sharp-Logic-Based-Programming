697b9116400a2f120a2c011605f714e558efeb2f
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-07-16 23:52:14 +0530

    Added Extract Digits Program

---
+++ Extract_Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿// LBP7
+
+// Extract Digits from the number
+
+// Implement a program to extract digits from the given number
+
+// input -------------> a number from the user
+// constraint --------> n>0
+// output ------------> print digits in line sep by space
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int d;
+
+        while (n != 0)
+        {
+            d = n % 10;
+            Console.Write(d + " ");
+            n = n / 10;
+        }
+    }
+}
