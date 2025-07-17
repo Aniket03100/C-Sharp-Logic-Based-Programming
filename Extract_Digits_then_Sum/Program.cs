2f8522cd904932e8be4cd85c15f327f3080886f8
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-07-17 23:53:49 +0530

    Added Extract Digits Then Sum Program

---
+++ Extract_Digits_then_Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿// LBP8
+
+// Sum of Digits 
+
+// Implement a program to calculate sum of digits present in the given number
+
+// input -------------> a number from the user
+// constraint --------> n>0
+// output ------------> print sum of digits
+
+
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int s, d;
+        s = 0;
+
+        while(n != 0)
+        {
+            d = n % 10;
+            s = s + d;
+            n = n / 10;
+
+        }
+        Console.WriteLine(s);
+    }
+}
