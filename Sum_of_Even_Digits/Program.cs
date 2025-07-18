a8822a20164d666be93ced7a871b922b20328936
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-07-18 23:53:39 +0530

    Added Sum of Even Digits

---
+++ Sum_of_Even_Digits/Program.cs
@@ -0,0 +1,29 @@
+﻿// LBP9
+
+// Sum of even Digits 
+
+// Implement a program to calculate sum of even digits present in the given number
+
+// input -------------> a number from the user
+// constraint --------> n>0
+// output ------------> print sum of even digits
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int s = 0;
+        int d;
+
+        while(n != 0)
+        {
+            d = n % 10;
+            if (n % 2 == 0)
+                s = s + d;
+            n = n / 10;
+        }
+        Console.WriteLine(s);
+    }
+}
